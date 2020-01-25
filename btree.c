b7df18b3d6792e9e61b33343ddb105644edef652
Anders J. Andersen <andersj.andersen@gmail.com>
2020-01-25 18:33:35 +0100

    Fixed the formatting of the code

--- btree.c
+++ btree.c
@@ -461,7 +461,7 @@ static BTREE_BOOL PushDown(DbFPtr db, int32_t Ref, DATAREC *data, DATAREC *promo
 {
     PAGEPTR        page,  newpage;
     BTREE_BOOL     found, promoted;
-    uint16_t          Slot;
+    uint16_t       Slot;
     static DATAREC p_b_data;
 
     if (Ref == 0)
@@ -530,12 +530,11 @@ static void InsInPage(DbFPtr db, DATAREC *data, PAGEPTR Page, uint16_t toSlot)
 static void Split(DbFPtr db, DATAREC *data, int32_t RightChild, PAGEPTR OldPage,
                   DATAREC *promotedData, PAGEPTR NewPage, uint16_t Slot)
 {
-    int   i;
+    int      i;
     uint16_t median;
-
     uint16_t minSlots;
     uint16_t order;
-    int bytesToHandle;
+    int      bytesToHandle;
 
     order = ORDER(db->RecLen);
 
@@ -657,7 +656,7 @@ BTREE_BOOL RemoveRecord(DbFPtr db, const char *record)
 static BTREE_BOOL RecDelete(DbFPtr db, DATAREC *data, int32_t Ref)
 {
     PAGEPTR    page;
-    uint16_t      pos;
+    uint16_t   pos;
     BTREE_BOOL found;
 
     if (Ref == 0)
@@ -909,7 +908,7 @@ BTREE_BOOL GetRecord(DbFPtr db, char *data)
     DATAREC    datarec;
     DATAREC    *dr;
     BTREE_BOOL found;
-    uint16_t      slot;
+    uint16_t   slot;
 
     if (db->Root == 0)
     {
